d69361bed5e10f64be98c0b73f06f95e57e6bbd0
taviyaumesh007 <88816691+taviyaumesh007@users.noreply.github.com>
2023-02-20 17:05:02 +0530

    beta-demo

--- demo/src/App.js
+++ demo/src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Subscription from "./components/Subscription";
 
 function App() {
+  let date = new Date("20,02,2023");
+  let title = "monthly subscription";
+  let amount = "130.5";
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Subscription
+        passedDate={date}
+        passedTitle={title}
+        passedAmount={amount}
+      />
     </div>
   );
 }
---
+++ demo/src/components/Subscription.js
@@ -0,0 +1,14 @@
+import "./Subscription.css";
+
+function Subscription() {
+  return (
+    <>
+      <div className="subscription">
+        <div>Date</div>
+        <h2 className="subscription_title">Title</h2>
+        <div className="subscription_price">Amount</div>
+      </div>
+    </>
+  );
+}
+export default Subscription;
